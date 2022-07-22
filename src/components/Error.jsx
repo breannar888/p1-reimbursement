cb89b825b8e8da47a8363b716ade69e5d781724a
breannar888 <67029835+breannar888@users.noreply.github.com>
2022-07-22 08:18:31 -0500

    add lottie animation on error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import errorPage from "../lottie/error_balloon.json";
+import Lottie from "lottie-react";
+import "../css/errorpage.css";
 
 export const Error = () => {
-  return <div>No Page Found!</div>;
+
+  return (
+    <div>
+      <Lottie className="animation" animationData={errorPage} loop={true} />
+    </div>
+    )
 };
